c45d16bbb50b83f504778c10eac05b38dbfb5e59
saviganga <akatugbasaviour@gmail.com>
2021-10-04 11:43:41 +0100

    inter-component communication via bindings and directives

--- src/app/artists/artists.components.ts
+++ src/app/artists/artists.components.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-artists',
@@ -6,5 +6,6 @@ import { Component } from "@angular/core";
   'styleUrls': ['./artists.component.css'],
 })
 export class ArtistsComponent {
+  @Input() artistsList: string[] = [];
 
 }
