81e450886ed5b871ffeaec1120a888d4ca8808bb
saviganga <akatugbasaviour@gmail.com>
2021-10-04 10:52:42 +0100

    created artists component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArtistsComponent } from './artists/artists.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ArtistsComponent,
   ],
   imports: [
     BrowserModule
